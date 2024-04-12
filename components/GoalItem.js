1d16e5e15ab30f8de6d5a344373c92e40ea30882
agpavlik <agpavlik@gmail.com>
2024-04-11 21:25:09 -0400

    Add Android Ripple Effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,13 +1,20 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
-//Pressable make omponent pressable
+//Pressable make component pressable
 
 function GoalItem(props) {
+  // bind method method creates a new function that, when called,
+  // calls this function with its this keyword set to the provided value,
+  // and a given sequence of arguments preceding any provided when the new function is called.
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210000" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -16,11 +23,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e4aaa",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
